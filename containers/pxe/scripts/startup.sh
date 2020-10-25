015ef1ab79336208ee0504c96d69a35a98e6b98c
Christian Hernandez <christian.hernandez@yahoo.com>
2020-10-25 15:47:07 -0700

    added wget to pxe and fixed syntax in script

--- containers/pxe/scripts/startup.sh
+++ containers/pxe/scripts/startup.sh
@@ -41,9 +41,9 @@ chmod 0555 ${rhcosDir}/{initramfs.img,kernel}
 
 #
 ## Create pxe/tftp files based on the template and yaml passed in.
-ansible localhost -c local -e @${helperPodYaml} -m template -a "src=${bootstrapPxeTemplate} dest="${pxeConfig}/01-{{ bootstrap.macaddr | lower | regex_replace (':', '-') }}" mode=0555" > ${ansibleLog} 2>&1
-ansible localhost -c local -e @${helperPodYaml} -m template -a "src=${masterPxeTemplate} dest="${pxeConfig}/01-{{ item.macaddr | regex_replace (':', '-')}}" mode=0555 with_items="{{ masters | lower }}"" > ${ansibleLog} 2>&1
-ansible localhost -c local -e @${helperPodYaml} -m template -a "src=${workerPxeTemplate} dest="${pxeConfig}/01-{{ item.macaddr | regex_replace (':', '-')}}" mode=0555 with_items="{{ workers | lower }}" when:"(workers is defined) and (workers | length > 0)"" > ${ansibleLog} 2>&1
+ansible localhost -c local -e @${helperPodYaml} -m template -a 'src=${bootstrapPxeTemplate} dest="${pxeConfig}/01-{{ bootstrap.macaddr | lower | regex_replace (':', '-') }}" mode=0555' > ${ansibleLog} 2>&1
+ansible localhost -c local -e @${helperPodYaml} -m template -a 'src=${masterPxeTemplate} dest="${pxeConfig}/01-{{ item.macaddr | regex_replace (':', '-')}}" mode=0555 with_items="{{ masters | lower }}"' > ${ansibleLog} 2>&1
+ansible localhost -c local -e @${helperPodYaml} -m template -a 'src=${workerPxeTemplate} dest="${pxeConfig}/01-{{ item.macaddr | regex_replace (':', '-')}}" mode=0555 with_items="{{ workers | lower }}" when:"(workers is defined) and (workers | length > 0)"' > ${ansibleLog} 2>&1
 
 #
 ## PXE is a "best effort" service that is kind of "old". So putting this here as a placeholder until someone has time to write a "checker"
