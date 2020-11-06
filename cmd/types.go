f75e4723d66c8f350e529535a5bb6e21cbf3541d
robertsandoval <rsandova@redhat.com>
2020-11-05 18:09:29 -0600

    added temporary values into type.go

--- cmd/types.go
+++ cmd/types.go
@@ -13,3 +13,12 @@ type Service struct {
 const QUAY string = "quay.io/helpernode"
 const DEFAULTTAG string = "latest"
 
+//TODO this needs to be removed and start/stop updated once we are reading from viper config file
+var images =  map[string]string{
+	"dns": "quay.io/helpernode/dns",
+	"dhcp": "quay.io/helpernode/dhcp",
+	"http": "quay.io/helpernode/http",
+	"loadbalancer": "quay.io/helpernode/loadbalancer",
+	"pxe": "quay.io/helpernode/pxe",
+}
+
